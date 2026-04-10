e1b218efcbc030b52cc57d6b741afce11a3bba4c
parimala <gparimala@email.com>
2026-04-10 15:41:57 -0400

    Added rebase feature

--- merge-rebase/app.py
+++ merge-rebase/app.py
@@ -1,9 +1,14 @@
 def app():
     print("Base version")
 
+
 def main_update():
     print("Main branch updated")
 
 def feature_merge():
     print("Feature from merge branch")
+
+
+def feature_rebase():
+    print("Feature from rebase branch")
 
